69c75ac14cec6499be5fd61f81918bda3898e59b
Johnny Johnson <johnny2136@gmail.com>
2017-02-03 18:12:52 -0500

    Added Files
    Version 1.00

---
+++ Web_Form/Script/entryApp.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var entryApp = angular.module('entryApp', []);
+
+function entryCtrl($scope, $http) {
+	var profile = {"firstName":"","lastName":""};
+
+        $scope.profile = profile
+
+	$scope.postData = function() {
+alert(JSON.stringify($scope.profile));
+
+        $http({
+            method: 'POST',
+	    data: JSON.stringify($scope.profile),
+	    contentType: "application/json",
+            url: 'https://api.mongolab.com/api/1/databases/apptest/collections/userProfiles?apiKey=sXsYo54JNRbcU9mm5RHrCGqd21hOY2lC'
+        }).success(function(data, status) {
+            alert("data was successfully added");
+        }).error(function(data, status, headers, config) {
+		alert("there was an error adding data");
+    });
+
+
+
+	}
+
+}
+
